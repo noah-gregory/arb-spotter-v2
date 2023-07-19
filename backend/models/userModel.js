0a0b04b15b5f7c11e7a54ce95c3ac306054f6cb1
noah-gregory <noah.gregory.fl@gmail.com>
2023-07-18 23:18:39 -0400

    created some directories

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,11 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: {
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
         type: String,
         required: true
     },
     email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
         type: String,
         required: true
     }
