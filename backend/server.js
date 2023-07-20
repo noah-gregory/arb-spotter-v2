2f1c00b0ea12cefd433bcb65eabff33bccb3de3d
JuddBE <jbrockedgar@gmail.com>
2023-07-19 23:07:01 -0400

    app.js

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const port = process.env.PORT || 5000;
-app.set('port', (process.env.PORT || 5000));
 const app = express();
-require('./database');
+// require('./database');
+app.set('port', (process.env.PORT || 5000));
 
 console.log(port);
 
@@ -13,15 +13,19 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const mongoose = require('mongoose');
+
 require('dotenv').config();
 const connection = process.env.MONGODB_URI;
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err));
-api.setApp( app, mongoose );
+
+
 // API
-const users = require('./api/users');
-app.use('./api/users', users);
+var api = require('./api.js');
+api.setApp( app, mongoose );
+// const users = require('./api/users');
+// app.use('./api/users', users);
 
 if (process.env.NODE_ENV === 'production'){
     app.use(express.static('../build'));
