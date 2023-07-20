c4089436560aa96c3e4833283ac792ddc4585cf8
JuddBE <jbrockedgar@gmail.com>
2023-07-19 20:17:00 -0400

    package-lock env fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from 'axios';
@@ -52,5 +52,5 @@ function App() {
 			</Routes>
 		</BrowserRouter>
 	);
-};
+}
 export default App
