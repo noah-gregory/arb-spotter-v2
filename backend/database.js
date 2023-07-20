a19103a1dc2016933f846b35762bd1707985ce4b
JuddBE <jbrockedgar@gmail.com>
2023-07-20 00:10:50 -0400

    FIXED??? AGAIN??

--- backend/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-const connection = process.env.MONGODB_URI;
-mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-    .then(() => console.log("Database Connected Successfully"))
-    .catch(err => console.log(err));
