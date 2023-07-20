282fc9851dbca6e8848409ec9a1b76b94ba62ad1
JuddBE <jbrockedgar@gmail.com>
2023-07-19 20:24:43 -0400

    package-lock env fix

--- src/App.js
+++ src/App.js
@@ -7,39 +7,6 @@ import SignupPage from './pages/SignupPage';
 import HomePage from './pages/HomePage';
 
 function App() {
-	// const [users, setUsers] = useState(null);
-
-	// const [username, setUsername] = useState("");
-	// const [email, setEmail] = useState("");
-	// useEffect(() => {
-	// 	axios
-	// 		.get("/api/users")
-	// 		.then((users) => setUsers(users))
-	// 		.catch((err) => console.log(err));
-	// }, []);
-
-	// function submitForm() {
-	// 	if (username === "") {
-	// 		alert("Please fill the username field");
-	// 		return;
-	// 	}
-	// 	if (email === "") {
-	// 		alert("Please fill the email field");
-	// 		return;
-	// 	}
-	// 	axios
-	// 		.post("/api/users", {
-	// 			username: username,
-	// 			email: email,
-	// 		})
-	// 		.then(function () {
-	// 			alert("Account created successfully");
-	// 			window.location.reload();
-	// 		})
-	// 		.catch(function () {
-	// 			alert("Could not creat account. Please try again");
-	// 		});
-	// }
 	return (
 		<BrowserRouter>
 			<Routes>
