55ef47a4263ad1d828214e2827f15045540aadc4
Joseph <JosephSchander@gmail.com>
2023-07-22 23:11:32 -0400

    Design tweaks

--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -7,19 +7,16 @@ const UploadPage = () => {
     return(
         (
                 <section className='Upload-Holder'>
-                    <section className='Title-Box'> 
-                        <input className='Title-Text' id='TitleText' type='text' placeholder='Insert Title' ></input>
-                    </section>
                     <section className='Image-Holder'>
                         <i className='Image-Icon'><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M512 144v288c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48h88l12.3-32.9c7-18.7 24.9-31.1 44.9-31.1h125.5c20 0 37.9 12.4 44.9 31.1L376 96h88c26.5 0 48 21.5 48 48zM376 288c0-66.2-53.8-120-120-120s-120 53.8-120 120 53.8 120 120 120 120-53.8 120-120zm-32 0c0 48.5-39.5 88-88 88s-88-39.5-88-88 39.5-88 88-88 88 39.5 88 88z"/></svg></i>
                         <button type='button' className='Upload-Button'><text className="upload-text">Upload Picture</text></button>
                     </section>
+                <button type="button"className="location-button">Select Location</button>
                 <span contentEditable='true'className='Caption-Text' id='CaptionText' type='text'></span>
                 <input maxLength='10'className='Tag-Button'placeholder="Add Tag"></input>
                 <input maxLength='10'className='Tag-Button'placeholder="Add Tag"></input>
                 <input maxLength='10'className='Tag-Button'placeholder="Add Tag"></input>
-                <input maxLength='10'className='Tag-Button'placeholder="Add Tag"></input>
-                <button type="button"className="finalize-button">Finalize Post</button>
+                <button type="button"className="finalize-button">Upload Post</button>
                 </section>
         )
     )
