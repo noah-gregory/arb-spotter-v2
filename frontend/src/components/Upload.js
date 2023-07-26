b33e39b5f09ed82720a7495899895a2992c31984
Joseph <JosephSchander@gmail.com>
2023-07-26 12:38:00 -0400

    Image Preview and Upload page css

--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -50,6 +50,19 @@ const UploadPage = () => {
     //   }
 
     const [file, setFile] = useState(null);
+    const [preview, setPreview] = useState();
+    useEffect(() => {
+        if (!file) {
+            setPreview(undefined)
+            return
+        }
+
+        const objectUrl = URL.createObjectURL(file)
+        setPreview(objectUrl)
+
+        // free memory when ever this component is unmounted
+        return () => URL.revokeObjectURL(objectUrl)
+    }, [file])
 
     const handleFileChange = (e) => {
         setFile(e.target.files[0]);
@@ -130,7 +143,9 @@ const UploadPage = () => {
             <section className='Upload-Holder'>
             <section className='Image-Holder'>
                 <i className='Image-Icon'><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M512 144v288c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48h88l12.3-32.9c7-18.7 24.9-31.1 44.9-31.1h125.5c20 0 37.9 12.4 44.9 31.1L376 96h88c26.5 0 48 21.5 48 48zM376 288c0-66.2-53.8-120-120-120s-120 53.8-120 120 53.8 120 120 120 120-53.8 120-120zm-32 0c0 48.5-39.5 88-88 88s-88-39.5-88-88 39.5-88 88-88 88 39.5 88 88z"/></svg></i>
-                <input type="file" onChange={handleFileChange}/>
+                {file && <img className='Image-Preview'src={preview} />}
+                <label for="file-upload" class="Upload-Button">Choose Image</label>
+                <input type="file" id='file-upload' onChange={handleFileChange}/>
             </section>
             <textarea contentEditable='true'className='Caption-Text' id='CaptionText' onChange={e => setCaption(e.target.value)} required type='text'maxLength={50} placeholder="Put Your Caption Here!
             (Max 50 characters)"></textarea>
